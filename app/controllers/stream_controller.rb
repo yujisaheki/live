2b53bb01c2070bb4af048cc81c69fad68fa6a53b
yujisaheki <yujisaheki@gmail.com>
2023-10-08 10:51:01 +0000

    stream

---
+++ app/controllers/stream_controller.rb
@@ -0,0 +1,5 @@
+class StreamController < ApplicationController
+  def index
+    render :index   
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #StreamController
+  get 'stream/index', to: 'stream#index', as: 'index_stream'
+
 end
